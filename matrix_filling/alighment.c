8dc88eb31e2e298c171a1e267e1413df31fd3e0b
Wenchang Zhang <npsfsz@gmail.com>
2017-04-12 00:59:44 -0400

    asd

--- matrix_filling/alighment.c
+++ matrix_filling/alighment.c
@@ -8,8 +8,8 @@
 #include <limits.h>
 #include "alighment.h"
 #include <cuda.h>
-#define MIN_SEQ_LEN 8//512
-#define MAX_SEQ_LEN 16//32768
+#define MIN_SEQ_LEN 4//512
+#define MAX_SEQ_LEN 8//32768
 
 
 static long max(long a, long b, long c)
